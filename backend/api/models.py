01ac4a09d15e2feb90bff3b8f825ca2f00d6c3f4
Theo <theo.delettre@gmail.com>
2025-06-14 14:42:43 +0100

    Added Tutorial Model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import uuid
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Tutorial(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tutorials')
+    transcript = models.JSONField()
+    video_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    body = models.JSONField()
+
+    def __str__(self):
+        return self.title
